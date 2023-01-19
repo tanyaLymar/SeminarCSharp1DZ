30cf7f738c966d11ee0a1e7e723cceb1f7e49d66
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-01-20 01:52:45 +0300

    Task3 solved

---
+++ task3/Program.cs
@@ -0,0 +1,6 @@
+﻿//Выяснить является ли число четным
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if (number%2 == 0) Console.WriteLine("Введенное число является четным");
+else Console.WriteLine("Введенное число является нечетным");
