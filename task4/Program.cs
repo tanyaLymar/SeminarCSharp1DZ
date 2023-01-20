97d67bed39c276a70a13f7e3ae7cc41d628b280d
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-01-20 21:38:18 +0300

    Task4 solved

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Показать четные числа от 1 до N
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int value = 1;
+
+if(value == 1)
+    {
+        value += 1;
+    } 
+
+while(value <=n)
+    {
+        Console.WriteLine(value);
+        value += 2;
+    }
+  
