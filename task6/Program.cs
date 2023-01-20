82f6cfb681cbd4777f3c903a418a54232eac822a
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-01-20 22:23:03 +0300

    Task6 solved

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int value = n/10;
+if (value >= 10 & value <= 99)
+    {
+    value = value%10;
+    Console.WriteLine("Вторая цифра введенного числа равна " + value);
+    }
+    else Console.WriteLine("Это не трехзначное число. Попробуйте еще раз");
