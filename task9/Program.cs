a5d0948249586f26db88ef4705b65fc132e4a5fd
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-01-20 23:11:07 +0300

    Task9 solved

---
+++ task9/Program.cs
@@ -0,0 +1,10 @@
+﻿//Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine() ?? "0");
+
+if(firstNumber%secondNumber == 0) Console.WriteLine(firstNumber + " кратно " +secondNumber);
+else Console.WriteLine("Остаток от деления равен " + firstNumber%secondNumber + ". Число " + firstNumber + " не кратно " + secondNumber);
+
